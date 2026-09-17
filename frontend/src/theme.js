09a8195025a56a8972226090f7aed676eb01d9e1
Arun Joseph <delvinjoseph07@gmail.com>
2026-09-17 15:53:04 +1000

    Enable Continuous Deployment and update frontend title

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,10 +3,10 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#1565C0",
+      main: "#17d8dfc2",
     },
     secondary: {
-      main: "#2E7D32",
+      main: "#5600f7",
     },
     background: {
       default: "#f5f5f5",
